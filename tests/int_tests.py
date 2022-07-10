ffd4ec7a4d14cebaac59d31318b6fbb5ac0a9de8
Alexandre Drummond <aogdrummond@gmail.com>
2022-07-10 13:17:32 -0300

    Inserting tests

---
+++ tests/int_tests.py
@@ -0,0 +1,95 @@
+"""
+Automatted integration tests. It needs connection with the database, since it tests
+the connections between db and factory.
+"""
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path(sys.path[0]).parent))
+
+from db_interface import mydb, dB_Cursor
+from datetime import datetime
+
+test_cursor = dB_Cursor()
+
+
+def test_connection_with_db():
+
+    assert mydb.is_connected()
+
+
+def test_create_table():
+
+    test_cursor.cursor.execute("USE bank_db")
+    test_cursor.cursor.execute("SHOW TABLES")
+    n_initial_tables = len(test_cursor.cursor.fetchall())
+
+    test_cursor.create_table(table_name="tab")
+    test_cursor.cursor.execute("SHOW TABLES")
+    n_final_tables = len(test_cursor.cursor.fetchall())
+    test_cursor.drop_table(table_name="tab")
+
+    assert n_initial_tables < n_final_tables
+
+
+def test_create_data():
+
+    test_cursor.create_table(table_name="tab")
+    test_cursor.create_data(table_name="tab")
+    test_cursor.cursor.execute("SELECT * FROM tab")
+    n_data = len(test_cursor.cursor.fetchall())
+
+    test_cursor.drop_table(table_name="tab")
+
+    assert n_data > 0
+
+
+def test_extract():
+
+    test_cursor.init_database_with_data()
+    mock_client_id = 1
+    expected_extract = [(10, datetime(2000, 10, 10, 22, 22, 22))]
+    received_extract = test_cursor.obtain_extract(mock_client_id)
+    test_cursor.clean_database()
+    assert received_extract == expected_extract
+
+
+def test_balance():
+
+    test_cursor.init_database_with_data()
+    mock_client_id = 1
+    expected_balance = 10
+    received_balance = test_cursor.calculates_balance(mock_client_id)
+    test_cursor.clean_database()
+    assert received_balance == expected_balance
+
+
+def test_wrong_extract():
+
+    test_cursor.init_database_with_data()
+    mock_client_id = 1
+    expected_extract = [(10, datetime(2022, 10, 10, 22, 22, 22))]
+    received_extract = test_cursor.obtain_extract(mock_client_id)
+    test_cursor.clean_database()
+    assert not received_extract == expected_extract
+
+
+def test_wrong_balance():
+
+    test_cursor.init_database_with_data()
+    mock_client_id = 1
+    expected_balance = 99
+    received_balance = test_cursor.calculates_balance(mock_client_id)
+    test_cursor.clean_database()
+    assert not received_balance == expected_balance
+
+
+if __name__ == "__main__":
+
+    test_connection_with_db()
+    test_create_table()
+    test_create_data()
+    test_extract()
+    test_balance()
+
+    print("All passed.")
--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -1,54 +1,89 @@
+"""
+Automatted unit tests. It needs connection with the database, since it uses
+the module "factory" to check its methods
+"""
+
 import sys
 from pathlib import Path
 
 sys.path.append(str(Path(sys.path[0]).parent))
-
-from db_interface import mydb, dB_Cursor
-
-cursor = dB_Cursor()
-
-
-def test_connection_with_db():
-
-    assert mydb.is_connected()
+from unittest import mock
+from factory import Client
+from utils import CPF_digits, get_smallest_notes_combination, is_valid_CPF
 
 
-def test_create_table():
+def test_CPF_digits_filtering():
 
-    cursor.execute("USE bank_db")
-    cursor.execute("SHOW TABLES")
-    n_initial_tables = len(cursor.fetchall())
+    CPF = [
+        ("999.999.999-99", "99999999999"),
+        ("  99999999999 ", "99999999999"),
+        ("99999999999", "99999999999"),
+    ]
 
-    cursor.create_table(cursor, table_name="tab")
-    cursor.execute("SHOW TABLES")
-    n_final_tables = len(cursor.fetchall())
-    cursor.drop_table(cursor, table_name="tab")
+    for cpf in CPF:
 
-    assert n_initial_tables < n_final_tables
+        assert CPF_digits(cpf[0]) == cpf[1]
 
 
-def test_create_data():
+def test_CPF_validation():
 
-    cursor.create_table(cursor, table_name="tab")
-    cursor.create_data(cursor, table_name="tab")
-    cursor.execute("SELECT * FROM tab")
-    n_data = len(cursor.fetchall())
+    CPF = "99999999999"
 
-    cursor.drop_table(cursor, table_name="tab")
+    assert is_valid_CPF(CPF) == True
 
-    assert n_data > 0
+
+def test_CPF_not_validation():
+
+    invalid_CPFs = ["9999999999", "999999999999", "s999999999", "test"]
+
+    for cpf in invalid_CPFs:
+        assert not is_valid_CPF(cpf) == True
+
+
+def test_right_notes_combination():
+
+    mock_data = [
+        (399, {"100": 3, "50": 1, "20": 2, "5": 1, "2": 2}),
+        (1000, {"100": 10}),
+        (123, {"100": 1, "20": 1, "2": 1, "1": 1}),
+    ]
+
+    for mock_notes in mock_data:
+        expected_notes = mock_notes[1]
+        received_notes = get_smallest_notes_combination(mock_notes[0])
+
+        assert received_notes == expected_notes
+
+
+# Dependency Injection to decouple from db for test
+class ClientMocker(Client):
+    def __init__(self, CPF):
+        self.CPF = CPF_digits(CPF)
+        self.id = 99
+        self.is_online = True
+
+
+def test_CPF_corresponds():
+
+    mock_data = [
+        ("99999999999", "99999999999"),
+        ("999.999.999-99", "99999999999"),
+        ("999 999 999 99", "99999999999"),
+    ]
+
+    for mock_CPF in mock_data:
+
+        client = ClientMocker(mock_CPF[0])
+        with mock.patch("builtins.input", return_value=mock_CPF[1]):
+            assert client.CPF_corresponds()
 
 
 if __name__ == "__main__":
 
-    test_connection_with_db()
-    test_create_table()
-    test_create_data()
+    test_CPF_digits_filtering()
+    test_CPF_validation()
+    test_CPF_not_validation()
+    test_right_notes_combination()
+    test_CPF_corresponds()
 
-    print("All passed")
-
-
-# Testar depositar valor negativo
-# Testar sacar valor negativo
-# Testar sacar mais que o saldo
-# Testar depositar valor char, bool, etc...
+    print("All tests passed.")
